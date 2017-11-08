4ab9e83983540518c1c05f7517c3a532644f978e
CodingJames <james.black.series@gmail.com>
2017-11-08 17:03:09 +0700

    Config connection.

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
-	$connect_db = mysql_connect("localhost","root","");
-	$db_name = "db_promotion";
+	$connect_db = mysql_connect("45.77.15.236","promotion_admin","promotion1234");
+	$db_name = "PromotionDB";
 	
 	if(!$connect_db){
 		echo "connect fail";
